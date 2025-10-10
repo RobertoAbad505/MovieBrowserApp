f87dd8eca50ea5aaf45d32c55b6249c96890ef87
Roberto Abad <roberto.rmzabad>
2025-10-09 19:27:15 -0600

    Initial commit

--- MovieBrowserApp/MovieBrowserAppApp.swift
+++ MovieBrowserApp/MovieBrowserAppApp.swift
@@ -25,7 +25,7 @@ struct MovieBrowserAppApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
         .modelContainer(sharedModelContainer)
     }
